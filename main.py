569b8bfc5d6a0fa0da983d2f3475610201101a52
Cas Olischläger <74181914+Toffeegaming@users.noreply.github.com>
2023-09-03 21:46:48 +0200

    reset with only classes

--- main.py
+++ main.py
@@ -2,9 +2,30 @@ from time import time
 from fastapi import FastAPI, __version__
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+from typing import Union, List, Tuple
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+# Classes
+class Player:
+    def __init__(self, name: str, mu: float, sigma: float):
+        self.name = name
+        self.mu = mu
+        self.sigma = sigma
+
+class Match:
+    def __init__(self, players: List[Player], result: List[int], new_rankings: List[Player] = []):
+        self.players = players
+        self.result = result
+        self.new_rankings = new_rankings
+
+
+# API endpoints
+@app.get("/")
+async def root():
+    return HTMLResponse(html)
 
 html = f"""
 <!DOCTYPE html>
@@ -25,10 +46,6 @@ html = f"""
     </body>
 </html>
 """
-
-@app.get("/")
-async def root():
-    return HTMLResponse(html)
 
 @app.get('/ping')
 async def hello():
