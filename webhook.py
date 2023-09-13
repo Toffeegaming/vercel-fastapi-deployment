9ccefe1cdfd08425fd50a8f86f667e23dffe9e3c
Cas Olischläger <74181914+Toffeegaming@users.noreply.github.com>
2023-09-13 22:10:02 +0200

    real webhook fix

--- webhook.py
+++ webhook.py
@@ -1,22 +1,24 @@
 from discord_webhook import AsyncDiscordWebhook
 from os import getenv
-
 async def PostWebhook(input):
+    print("Posting webhook...")
     webhookUrl = getenv('DISCORD_WEBHOOK_URL')
     try:
-        player1 = input['players']['1']['name']
-        player2 = input['players']['2']['name']
-        player3 = input['players']['3']['name']
-        player4 = input['players']['4']['name']
-        result = input['result']
-        if result == 1:
+        player1 = input[0][2][0]
+        player2 = input[0][2][1]
+        player3 = input[0][2][2]
+        player4 = input[0][2][3]
+        result = input[0][1]
+        if result == [0,1]:
             webhookContent = f"{player1} en {player2} hebben gewonnen tegen {player3} en {player4}"
-        elif result == 2:
+        elif result == [1,0]:
             webhookContent = f"{player3} en {player4} hebben gewonnen tegen {player1} en {player2}"
         else:
             webhookContent = f"{player1} en {player2} hebben gelijk gespeeld tegen {player3} en {player4}"
-    except:
+    except Exception as e:
         webhookContent = "Fout in data!"
-
-    webhook = AsyncDiscordWebhook(url=webhookUrl, content=webhookContent)
-    await webhook.execute()
+        print(e)
+    finally:
+        webhook = AsyncDiscordWebhook(url=webhookUrl, content=webhookContent)
+        await webhook.execute()
+        print("Webhook posted!")
